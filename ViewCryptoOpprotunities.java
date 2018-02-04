ecbcb81303a418d37c2a1766ff7898bc48f478c6
Alex Koby <alex.r.koby@gmail.com>
2018-02-04 17:42:04 -0500

    Makes calls to arbitrageFinder and println the opprotunities that meet user's standards

--- ViewCryptoOpprotunities.java
+++ ViewCryptoOpprotunities.java
@@ -6,30 +6,44 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.view.View;
 import android.util.Log;
-
+import java.util.PriorityQueue;
 /**
  * Created by Alexander on 1/7/2018.
  */
 
 public class ViewCryptoOpprotunities extends Activity{
-
-
+    ArbitrageFinder arbitrageFinder;
+    PriorityQueue<Opportunity> bestOpportunitiesWithinExchanges;
+    PriorityQueue<Opportunity> bestOpportunitiesAcrossExchanges;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crypto_opprotunities);
 
-        //addAsksAndBidsToCoins(HomePage.taskBitfinex, HomePage.bitfinex);
-        //addAsksAndBidsToCoins(HomePage.taskBitStamp, HomePage.bitStamp);
-        //addAsksAndBidsToCoins(HomePage.taskOKEX, HomePage.OKEX);
-        addAsksAndBidsToCoins(HomePage.taskGDAX, HomePage.GDAX);
-        //System.out.println("Homepage: " + MainActivity.isCreatedHomepage + "CryptoPage " +
-        //        MainActivity.isCreatedCryptocurrencies + " Exchanges: " + MainActivity.isCreatedExchanges);
-
-        System.out.print("Homepage size is: " + HomePage.taskBittrex.getExchangeCoinsAskUSD().size());
-        System.out.println("AKA: " + HomePage.bittrex.getCoins().size());
+        arbitrageFinder = new ArbitrageFinder(HomePage.minGainsWanted);
+        if(HomePage.typeOfArbitrageString.equals("Inter-Exchange and Cross Exchange Arbitrage")) {
+            bestOpportunitiesAcrossExchanges = arbitrageFinder.getBestOpportunitiesAcrossExchange();
+            bestOpportunitiesWithinExchanges = arbitrageFinder.getBestOpportunitiesWithinExchange();
+        }
+        else if (HomePage.typeOfArbitrageString.equals("Inter-Exchange Arbitrage Only")) {
+            bestOpportunitiesWithinExchanges = arbitrageFinder.getBestOpportunitiesWithinExchange();
+        }
+        else {
+            bestOpportunitiesAcrossExchanges = arbitrageFinder.getBestOpportunitiesAcrossExchange();
+        }
+        while(bestOpportunitiesAcrossExchanges.size() > 0){
+            System.out.println(bestOpportunitiesAcrossExchanges.peek().getPercentGain() + "% at " +
+                    bestOpportunitiesAcrossExchanges.peek().getLowPriceCoinExchange().getExchange() + " to" +
+            bestOpportunitiesAcrossExchanges.peek().getHighPriceCoinExchange().getExchange() + " Coin is : " +
+            bestOpportunitiesAcrossExchanges.poll().getHighPriceCoinExchange().getName());
+        }
+        while(bestOpportunitiesWithinExchanges.size() > 0){
+            System.out.println(bestOpportunitiesWithinExchanges.peek().getPercentGain() + "% at " +
+                    bestOpportunitiesWithinExchanges.peek().getHighPriceCoinExchange().getName()+ " Coin is : " +
+                    bestOpportunitiesWithinExchanges.peek().getHighPriceCoinExchange().getExchange() + " Type" +
+            bestOpportunitiesWithinExchanges.poll().getType());
+        }
     }
-
 
     /**
      * Transfers all of the data from the download task to the exchange
@@ -37,7 +51,7 @@ public class ViewCryptoOpprotunities extends Activity{
      * @param e is the exchange
      */
     public void addAsksAndBidsToCoins(DownloadTask dt1, Exchange e){
-        System.out.println(e.getName());
+        System.out.println(e.getName() + " BID USD " + dt1.getExchangeCoinsBidUSD().size());
         for(Coin c1: e.getCoins()){
             int index = e.getCoins().indexOf(c1);
             c1.setBidPriceUSD(dt1.getExchangeCoinsBidUSD().get(index));
@@ -50,7 +64,6 @@ public class ViewCryptoOpprotunities extends Activity{
             System.out.println(c1.getName() + " " + c1.getAskPriceUSD() + " " + c1.getAskPriceBTC() + " " + c1.getAskPriceETH());
 
         }
-
     }
 
 
