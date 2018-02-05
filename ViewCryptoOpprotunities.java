d92a28fb383560f95e191697852cd6f3131a3616
Alex Koby <alex.r.koby@gmail.com>
2018-02-05 18:40:43 -0500

    Deleted getAsksAndBids method as it is no longer needed

--- ViewCryptoOpprotunities.java
+++ ViewCryptoOpprotunities.java
@@ -45,26 +45,6 @@ public class ViewCryptoOpprotunities extends Activity{
         }
     }
 
-    /**
-     * Transfers all of the data from the download task to the exchange
-     * @param dt1 is the download task
-     * @param e is the exchange
-     */
-    public void addAsksAndBidsToCoins(DownloadTask dt1, Exchange e){
-        System.out.println(e.getName() + " BID USD " + dt1.getExchangeCoinsBidUSD().size());
-        for(Coin c1: e.getCoins()){
-            int index = e.getCoins().indexOf(c1);
-            c1.setBidPriceUSD(dt1.getExchangeCoinsBidUSD().get(index));
-            c1.setAskPriceUSD(dt1.getExchangeCoinsAskUSD().get(index));
-            c1.setBidPriceBTC(dt1.getExchangeCoinsBIDBTC().get(index));
-            c1.setAskPriceBTC(dt1.getExchangeCoinsAskBTC().get(index));
-            c1.setBidPriceETH(dt1.getExchangeCoinsBIDETH().get(index));
-            c1.setAskPriceETH(dt1.getExchangeCoinsAskETH().get(index));
-
-            System.out.println(c1.getName() + " " + c1.getAskPriceUSD() + " " + c1.getAskPriceBTC() + " " + c1.getAskPriceETH());
-
-        }
-    }
 
 
 
